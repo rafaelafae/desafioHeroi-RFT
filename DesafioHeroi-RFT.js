f9657880995ddadd9ada24d770ba15017b987296
rafaelafae <rafaela.fae@gmail.com>
2023-12-27 19:51:51 -0300

    Envio 06

--- DesafioHeroi-RFT.js
+++ DesafioHeroi-RFT.js
@@ -2,12 +2,24 @@
 console.log("Bem-vindo ao Desafio Herói realizado pela Rafaela Faé");
 console.log("O objetivo deste desafio é testar os conhecimentos adquiridos na linguagem JavaScript. Utilizando variáveis, operadores, laços de repetição e estruturas de decisão");
 
+// Variáveis - Jogadores
+let nick0 = "Faekinha";
+let tipoNick0 = "elfa arqueira";
+let xpNick0 = 8934;
+let warNick0 = true;
 
-// Tela de início do jogo
-function prompt(message = "Seja bem-vindo ao RPG 2023, digite um nickname") {
-  return window.prompt(message);
-}
+let nickname1 = "Avril";
+let tipoNick1 = "orc buffer";
+let xpNick1 = 7687;
+let warNick1 = false;
 
-// Exemplo de uso
-let nickname0 = prompt();
-console.log(`Bem-vindo, ${nickname0}!`);
+let nickname2 = "Sarah";
+let tipoNick2 = "dark elfa maga";
+let xpNick2 = 1362;
+let warNick2 = false;
+
+let nickname3 = "Kenshin";
+let tipoNick3 = "anão artesão";
+let xpNick3 = 10795;
+let warNick3 = false;
+
