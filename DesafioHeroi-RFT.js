deed3d0ecb49bca7a799ea48fae000078a237e87
rafaelafae <rafaela.fae@gmail.com>
2023-12-27 19:14:31 -0300

    Envio 03

--- DesafioHeroi-RFT.js
+++ DesafioHeroi-RFT.js
@@ -1,1 +1,4 @@
-console.log ("teste")
+//Desafio Heroí - DIO - Rafaela Faé Theodoro
+console.log ("Bem vindo ao Desafio Heroí realizado pela Rafaela Faé")
+console.log ("Obejetivo deste desafio, é testar os conhecimentos adiquiridos na linguagem JavaScript. Utilizando variáveis, operadores, laços de repetição e estruturas de decisão")
+//Declarar variáveis
