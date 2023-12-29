20d3c5e4cf1b1f7f9c93c7342204a7adf4086575
rafaelafae <rafaela.fae@gmail.com>
2023-12-29 20:54:42 -0300

    Envio Final

--- DesafioHeroi-RFT.js
+++ DesafioHeroi-RFT.js
@@ -8,7 +8,7 @@ console.log ("Apresento 5 participantes, time de fighters, com suas raças e cla
 console.log(" ")
 
 //Classes permitidas para lutar
-console.log ("As classes, independente de suas raças, convocadas para lutar na guerra, são: arqueiros, daggers, magos, tankers, bladedancers e swordsingers")
+console.log ("As classes, independente de suas raças, convocadas para lutar na guerra, são: arqueiros, daggers, magos e tankers")
 console.log(" ")
 
 //Divisão de níveis
@@ -20,9 +20,11 @@ let players = [
     ["Faekinha", "elfo arqueiro", 8934, true],
     ["Avril", "orc buffer", 9687, false],
     ["Sushi", "elfo tanker", 12843, true],
-    ["Pedrita", "humano mago", 5350, false],
+    ["Pedrita", "humano mago", 5350, true],
     ["Nina", "elfo swordsinger", 3650, false],
 ]
+
+console.log("Classificação na data da inscrição")
 //Dia de inscrição - classificação
 for (let i = 0; i < players.length; i++) {
     console.log(
@@ -46,41 +48,39 @@ for (let i = 0; i < players.length; i++) {
         console.log("classificado no nível: Radiante");
     }
 }
-
-//Como alguns jogadores não gostaram da sua classificação, os mesmos decidiram subir de níveis para o dia da luta
+console.log(" ")
+console.log("Classificação na data de luta")
+// Como alguns jogadores não gostaram da sua classificação, os mesmos decidiram subir de níveis para o dia da luta
 for (let i = 0; i < players.length; i++) {
     if (players[i][0] === "Faekinha") {
-        players[i][2] = 9560;
+        for (let Faekinha = 8934; Faekinha < 9560; Faekinha++) {
+            players[i][2] = Faekinha;
+                   }
     } else if (players[i][0] === "Pedrita") {
-        players[i][2] = 8430;
+        for (let Pedrita = 5350; Pedrita < 8430; Pedrita++) {
+            players[i][2] = Pedrita;
+                    }
     }
 }
 
-let players1 = [
-    ["Faekinha", "elfo arqueiro", 9560, true],
-    ["Avril", "orc buffer", 9687, false],
-    ["Sushi", "elfo tanker", 12843, true],
-    ["Pedrita", "humano mago", 8430, true],
-    ["Nina", "elfo swordsinger", 3650, false],
-]
-//Dia de luta - Classificação para lutar
-for (let i = 0; i < players1.length; i++) {
+// Dia de luta - Classificação para lutar
+for (let i = 0; i < players.length; i++) {
     console.log(
-        "O player " + players1[i][0] + ", raça e classe de " + players1[i][1] + " está apto para a luta? " + players1[i][3]
+        "O player " + players[i][0] + ", raça e classe de " + players[i][1] + " está apto para a luta? " + players[i][3]
     );
-    if (players1[i][3] === true && players1[i][2] < 1000) {
+    if (players[i][3] === true && players[i][2] < 1000) {
         console.log("classificado no nível: Ferro");
-    } else if (players1[i][2] >= 1001 && players1[i][2] <= 2000) {
+    } else if (players[i][2] >= 1001 && players[i][2] <= 2000) {
         console.log("classificado no nível: Bronze");
-    } else if (players1[i][2] >= 2001 && players1[i][2] <= 5000) {
+    } else if (players[i][2] >= 2001 && players[i][2] <= 5000) {
         console.log("classificado no nível: Prata");
-    } else if (players1[i][2] >= 6001 && players1[i][2] <= 7000) {
+    } else if (players[i][2] >= 6001 && players[i][2] <= 7000) {
         console.log("classificado no nível: Ouro");
-    } else if (players1[i][2] >= 7001 && players1[i][2] <= 8000) {
+    } else if (players[i][2] >= 7001 && players[i][2] <= 8000) {
         console.log("classificado no nível: Platina");
-    } else if (players1[i][2] >= 8001 && players1[i][2] <= 9000) {
+    } else if (players[i][2] >= 8001 && players[i][2] <= 9000) {
         console.log("classificado no nível: Ascendente");
-    } else if (players1[i][2] >= 9001 && players1[i][2] <= 10000) {
+    } else if (players[i][2] >= 9001 && players[i][2] <= 10000) {
         console.log("classificado no nível: Imortal");
     } else {
         console.log("classificado no nível: Radiante");
