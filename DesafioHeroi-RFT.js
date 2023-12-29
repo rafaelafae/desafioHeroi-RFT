29e91b9cf2bafc0571d9ee512c2a4eb55b8dbad7
rafaelafae <rafaela.fae@gmail.com>
2023-12-29 20:19:39 -0300

    Envio 08

--- DesafioHeroi-RFT.js
+++ DesafioHeroi-RFT.js
@@ -1,24 +1,30 @@
-// Desafio Herói - DIO - Rafaela Faé Theodoro
-console.log("Bem-vindo ao Desafio Herói realizado pela Rafaela Faé");
-console.log("O objetivo deste desafio é testar os conhecimentos adquiridos na linguagem JavaScript. Utilizando variáveis, operadores, laços de repetição e estruturas de decisão");
-
-// Variáveis - Jogadores
-let nick0 = "Faekinha";
-let tipoNick0 = "elfa arqueira";
-let xpNick0 = 8934;
-let warNick0 = true;
-
-let nickname1 = "Avril";
-let tipoNick1 = "orc buffer";
-let xpNick1 = 7687;
-let warNick1 = false;
-
-let nickname2 = "Sarah";
-let tipoNick2 = "dark elfa maga";
-let xpNick2 = 1362;
-let warNick2 = false;
-
-let nickname3 = "Kenshin";
-let tipoNick3 = "anão artesão";
-let xpNick3 = 10795;
-let warNick3 = false;
+// Variáveis - Players - (Nickname, raça e classe, nível e se esta apto ou não para a guerra)
+let players = [
+    ["Faekinha", "elfo arqueiro", 8934, true],
+    ["Avril", "orc buffer", 9687, false],
+    ["Sushi", "elfo tanker", 12843, true],
+    ["Pedrita", "humano mago", 5350, false],
+    ["Nina", "elfo swordsinger", 3650, false],
+]
+for (let i = 0; i < players.length; i++) {
+    console.log(
+        "O player " + players[i][0] + ", raça e classe de " + players[i][1] + " está apto para a luta? " + players[i][3]
+    );
+    if (players[i][3] === true && players[i][2] < 1000) {
+        console.log("classificado no nível: Ferro");
+    } else if (players[i][2] >= 1001 && players[i][2] <= 2000) {
+        console.log("classificado no nível: Bronze");
+    } else if (players[i][2] >= 2001 && players[i][2] <= 5000) {
+        console.log("classificado no nível: Prata");
+    } else if (players[i][2] >= 6001 && players[i][2] <= 7000) {
+        console.log("classificado no nível: Ouro");
+    } else if (players[i][2] >= 7001 && players[i][2] <= 8000) {
+        console.log("classificado no nível: Platina");
+    } else if (players[i][2] >= 8001 && players[i][2] <= 9000) {
+        console.log("classificado no nível: Ascendente");
+    } else if (players[i][2] >= 9001 && players[i][2] <= 10000) {
+        console.log("classificado no nível: Imortal");
+    } else {
+        console.log("classificado no nível: Radiante");
+    }
+}
